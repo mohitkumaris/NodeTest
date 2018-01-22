c74b0949be32b4806e63d9173d43ad2695b9b919
mohitkumaris <mohitkumaris@gmail.com>
2018-01-22 22:41:06 +0530

    expect js for assertions

--- node-test/utils/utils.js
+++ node-test/utils/utils.js
@@ -1,8 +1,38 @@
 const add=(a,b)=>a+b;
 const square=(x)=>x*x;
+let setName=(user,fullname)=>{
+
+    let name=fullname.split(' ');
+    user.FirstName=name[0];
+    user.LastName=name[1];
+   return user;
+};
+
+let addAsync=(a,b,callback)=>{
+
+    setTimeout(()=>{
+
+        callback(a+b);
+
+    },1000)
+
+};
+
+var squareAsync=(a,callback)=>{
+
+    setTimeout(()=>{
+
+        callback(a*a);
+
+    },1000)
+
+}
 
 module.exports={
 
     add,
-    square
+    square,
+    setName,
+    addAsync,
+    squareAsync
 };
--- node-test/utils/utils.test.js
+++ node-test/utils/utils.test.js
@@ -1,23 +1,65 @@
-
+const expect=require('expect');
 const util=require('./utils');
+
 
 // mocha behaves on BDD (Behaviour driven Development)
 it('should add two numbers',()=>{
 
     let res=util.add(22,4);
 
-    if(res != 26){
-
-        throw new Error(`Added numbers should be 26 but its ${res}`);
-    }
+    expect(res).toBe(26).toBeA('number');
 });
 
 it('should be square of two numbers',()=>{
 
     let sq=util.square(5);
 
-    if(sq != 25){
+    expect(sq).toBe(25).toBeA('number');
+});
 
-        throw new Error(`Square should be 25 but its ${sq}`);
-    }
+it('first name and last name should be there',()=>{
+
+    var user={ FirstName:'Mohit',
+        LastName:'Kumar'};
+    let names=util.setName(user,'Mohit Kumar');
+
+    expect(names).toInclude({
+        FirstName:'Mohit',
+        LastName:'Kumar'
+    })
+
+});
+
+/*
+For Async we should always use done as Mocha
+dont run if test case takes more than 2 seconds
+and as it is async the callback of it runs
+before the result is executed in callback.
+ */
+it('Numbers will be added in Async',(done)=>{
+
+    util.addAsync(4,10,(sum)=>{
+
+        expect(sum).toBe(14).toBeA('number');
+        done();
+    });
+
+});
+
+it('Numbers will be squared in Async',(done)=>{
+
+    util.squareAsync(4,(square)=>{
+
+        expect(square).toBe(16).toBeA('number');
+        done();
+    });
+
+});
+
+it('should expect some values',()=>{
+
+    expect(12).toNotBe(11);
+    expect({name:'mohit'}).toEqual({name:'mohit'});
+    expect([2,3,4]).toExclude(5);
+
 });
